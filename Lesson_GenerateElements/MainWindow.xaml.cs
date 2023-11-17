a713ccc229b86214070dc146e7735994b3072729
Stranik2504 <58374405+Stranik2504@users.noreply.github.com>
2023-11-17 20:03:16 +0300

    Create Object from code and Move object

---
+++ Lesson_GenerateElements/MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Lesson_GenerateElements
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Button button = new Button();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            button.Background = Brushes.Yellow;
+            button.Content = "Click ME!";
+            button.Width = 350;
+            button.Height = 100;
+            button.FontSize = 30;
+
+            // add event
+            button.Click += Button_Click;
+
+            Grid1.Children.Add(button);
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            button.Background = Brushes.Green;
+        }
+    }
+}
---
+++ Lesson_MoveObject/MainWindow.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Lesson_MoveObject
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void MyCanvas_KeyDown(object sender, KeyEventArgs e)
+        {
+            // Down
+            if (e.Key == Key.S && Canvas.GetTop(rectangle) + rectangle.Height < 410)
+            {
+                Canvas.SetTop(rectangle, Canvas.GetTop(rectangle) + 10);
+            }
+
+            // Up
+            if (e.Key == Key.W && Canvas.GetTop(rectangle) > 0)
+            {
+                Canvas.SetTop(rectangle, Canvas.GetTop(rectangle) - 10);
+            }
+
+            // Right
+            if (e.Key == Key.D && Canvas.GetLeft(rectangle) + rectangle.Width < 790)
+            {
+                Canvas.SetLeft(rectangle, Canvas.GetLeft(rectangle) + 10);
+            }
+
+            // Left
+            if (e.Key == Key.A && Canvas.GetLeft(rectangle) > 0)
+            {
+                Canvas.SetLeft(rectangle, Canvas.GetLeft(rectangle) - 10);
+            }
+        }
+    }
+}
