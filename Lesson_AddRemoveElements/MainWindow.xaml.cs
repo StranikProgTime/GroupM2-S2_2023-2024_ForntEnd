4a60e4926c13cfac3a17a8863c3daabf588ceebf
Stranik2504 <58374405+Stranik2504@users.noreply.github.com>
2023-11-24 19:56:24 +0300

    Add TimerApp and start work in AddRemoveElements

---
+++ Lesson_AddRemoveElements/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Lesson_AddRemoveElements
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void MyCanvas_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            Rectangle rectangle = new Rectangle();
+
+            rectangle.Width = 50;
+            rectangle.Height = 50;
+            rectangle.Fill = Brushes.Blue;
+
+            Canvas.SetTop(rectangle, e.GetPosition(MyCanvas).Y - rectangle.Height / 2);
+            Canvas.SetLeft(rectangle, e.GetPosition(MyCanvas).X - rectangle.Width / 2);
+
+            MyCanvas.Children.Add(rectangle);
+        }
+    }
+}
---
+++ Lesson_TimerApp/MainWindow.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+using System.Windows.Threading;     // for timer
+
+namespace Lesson_TimerApp
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        // fields
+        int left = 0;
+        int speed = 10;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            left = (int)Ball.Margin.Left;
+
+            DispatcherTimer timer = new DispatcherTimer();
+            timer.Interval = TimeSpan.FromMilliseconds(100);
+
+            timer.Tick += Timer_Tick;
+
+            timer.Start();
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            left += speed;
+            Ball.Margin = new Thickness(left, 0, 0, 20);
+
+            if (Ball.Margin.Left > 800 - Ball.Width - 20)
+            {
+                speed = -5;
+            }
+
+            if (Ball.Margin.Left < 0)
+            {
+                speed = 5;
+            }
+        }
+    }
+}
