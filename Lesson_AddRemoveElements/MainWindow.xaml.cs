405a1e411c4d16821d29edf719a9627d12c4ed57
Stranik2504 <58374405+Stranik2504@users.noreply.github.com>
2023-12-03 19:34:52 +0300

    End work with Lesson_AddRemoveElements and start work with project Lesson_PlatformerApp

--- Lesson_AddRemoveElements/MainWindow.xaml.cs
+++ Lesson_AddRemoveElements/MainWindow.xaml.cs
@@ -13,6 +13,8 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
+using System.Windows.Threading; // for timer
+
 namespace Lesson_AddRemoveElements
 {
     /// <summary>
@@ -20,23 +22,49 @@ namespace Lesson_AddRemoveElements
     /// </summary>
     public partial class MainWindow : Window
     {
+        List<Rectangle> rectangles = new List<Rectangle>();
+
         public MainWindow()
         {
             InitializeComponent();
+
+            DispatcherTimer timer = new DispatcherTimer();
+            timer.Interval = TimeSpan.FromMilliseconds(10);
+            timer.Tick += Timer_Tick;
+            timer.Start();
         }
 
         private void MyCanvas_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
         {
-            Rectangle rectangle = new Rectangle();
+            if (e.Source is Canvas) // is - проверка является ли объект типом данных
+            {
+                Rectangle rectangle = new Rectangle();
 
-            rectangle.Width = 50;
-            rectangle.Height = 50;
-            rectangle.Fill = Brushes.Blue;
+                rectangle.Width = 50;
+                rectangle.Height = 50;
+                rectangle.Fill = Brushes.Blue;
 
-            Canvas.SetTop(rectangle, e.GetPosition(MyCanvas).Y - rectangle.Height / 2);
-            Canvas.SetLeft(rectangle, e.GetPosition(MyCanvas).X - rectangle.Width / 2);
+                Canvas.SetTop(rectangle, e.GetPosition(MyCanvas).Y - rectangle.Height / 2);
+                Canvas.SetLeft(rectangle, e.GetPosition(MyCanvas).X - rectangle.Width / 2);
 
-            MyCanvas.Children.Add(rectangle);
+                MyCanvas.Children.Add(rectangle);
+
+                rectangles.Add(rectangle);
+            }    
+            else
+            {
+                MyCanvas.Children.Remove((Rectangle)e.Source);
+
+                rectangles.Remove((Rectangle)e.Source);
+            }
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            for (int i = 0; i < rectangles.Count; i++)
+            {
+                Canvas.SetLeft(rectangles[i], Canvas.GetLeft(rectangles[i]) + 5);
+            }
         }
     }
 }
---
+++ Lesson_PlatformerApp/MainWindow.xaml.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+using System.Diagnostics;   // for Debug
+
+namespace Lesson_PlatformerApp
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        // fields
+        private bool bLeft;
+        private bool bRight;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void MyCanvas_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.A)
+            {
+                bLeft = true;
+                Debug.WriteLine("Start move left");
+            }
+            else if (e.Key == Key.D)
+            {
+                bRight = true;
+                Debug.WriteLine("Start move right");
+            }
+        }
+
+        private void MyCanvas_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.A)
+            {
+                bLeft = false;
+                Debug.WriteLine("End move left");
+            }
+            else if (e.Key == Key.D)
+            {
+                bRight = false;
+                Debug.WriteLine("End move right");
+            }
+        }
+    }
+}
